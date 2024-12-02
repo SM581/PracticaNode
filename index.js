c469fbf2a6b08628a647c7d6bd27d807b8a5a8e6
SM581 <148915149+SM581@users.noreply.github.com>
2024-12-02 09:27:04 -0500

    Hola Mundo_Node

---
+++ index.js
@@ -0,0 +1,16 @@
+//protocolo de intercambio
+var http=require('http');
+//creando servidor mediante http
+var server=http.createServer();
+
+//servicio web
+function mensaje(req,resp){
+    resp.writeHead(200,{'content-type':'text/plain'});
+    resp.write('hola mundo');
+    resp.end();
+}
+
+server.on('request',mensaje);
+server.listen(3000,function(){
+    console.log("la solicitud fue realizada mediate el puerto 3000");
+});
